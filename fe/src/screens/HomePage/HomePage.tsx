583cbd27124628804e31bce519d13eabe7a5ad37
Vilém Raška <wilhelm.master@gmail.com>
2021-10-11 00:57:51 +0200

    Sync to server

--- fe/src/screens/HomePage/HomePage.tsx
+++ fe/src/screens/HomePage/HomePage.tsx
@@ -3,6 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { GoodsInCartProvider } from 'contexts/GoodsInCart';
 import GoodsOffer from 'components/GoodsOffer/GoodsOffer';
 import Cart from 'components/Cart/Cart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faBox } from '@fortawesome/free-solid-svg-icons';
 
 const HomePage: React.FC = () => {
 	return(
@@ -10,12 +12,18 @@ const HomePage: React.FC = () => {
 			<Container>
 				<Row xs={1} md={2}>
 					<Col xs={{order: 'last'}} md={{order: 'first'}} className="mb-4">
-						<h3 className="mb-3">Zboží v nabídce</h3>
+						<h3 className="mb-3">
+							<FontAwesomeIcon icon={faBox} className="mr-3"/>
+							Zboží v nabídce
+						</h3>
 						<GoodsOffer/>
 					</Col>
 
 					<Col xs={{order: 'first'}} md={{order: 'last'}} className="mb-4">
-						<h3 className="mb-3">Nákupní košík</h3>
+						<h3 className="mb-3">
+							<FontAwesomeIcon icon={faShoppingCart} className="mr-3"/>
+							Nákupní košík
+						</h3>
 						<Cart/>
 					</Col>
 				</Row>
